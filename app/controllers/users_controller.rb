b2dd01ae144dcc006fa7c963cd6cfa6b333ec379
Sefa ŞAHİN <sefa@dehaa.com>
2017-03-05 13:19:54 +0300

    user ekleme işlemleri

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit!)
+    if @user.save
+      flash[:notice] = "Aramıza Hoş Geldin"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit!
+  end
 end
